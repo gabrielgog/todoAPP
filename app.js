28787f3f611c18e1d35a9cceef789d840777f3f8
gabrielgog <gabrielgog96@gmail.com>
2020-10-03 11:44:09 +0100

    add ejs template

--- app.js
+++ app.js
@@ -1,58 +1,46 @@
-// jshint esversiion:6
+// jshint esversion: 6
 
 const express = require("express")
+
 const bodyParser = require("body-parser")
 
-const app = express()
+const app = express();
 
 app.set('view engine', 'ejs');
+
+
 
 app.get("/", function (req, res) {
 
     var today = new Date();
+
     var currentDay = today.getDay();
-    var day = "";
+
+    var day = ""
 
     switch (currentDay) {
         case 0:
-
             day = "Sunday"
-
-            break;
-        case 1:
-
-            day = "Monday"
-
-            break;
-        case 2:
-
-            day = "Tuesday"
-
-            break;
-        case 3:
-
-            day = "Wednesday"
-
+            
             break;
 
-        case 4:
+            case 6:
 
-            day = "Thursday"
+            day = "Saturday"
 
             break;
-
+    
         default:
             break;
     }
 
-    res.render("list", { KindOfDay: day })
+        
+    res.render("list", { kindOfDay: day })
 
 
-});
-
-
+})
 
 
 app.listen(3000, function () {
     console.log("server is running on port 3000")
-});
+})
