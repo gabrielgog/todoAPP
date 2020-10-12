51860e37788e4235421371a34402846b7ab55742
gabrielgog <gabrielgog96@gmail.com>
2020-10-12 02:32:54 +0100

    add css stylesheet

--- app.js
+++ app.js
@@ -5,29 +5,29 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express(); 
-var values = [];
+let values = [];
 
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
-
+app.use(express.static('public'))
 app.get("/", function(req, res){
 
 
 
 
-var today = new Date()
+let today = new Date()
 
 
 
-var options = {
+let options = {
     weekday: 'long', 
     day: 'numeric',
 
     month: 'long'
 }
 
-var day = (today.toLocaleDateString("en-US", options))
+let day = (today.toLocaleDateString("en-US", options))
 
 res.render("list", {kindOfDay: day, newListItems: values})
 
